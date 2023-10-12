8b7dae41382e3baa55189698bf2f8b6c46574204
felipe-17y <131884999+felipe-17y@users.noreply.github.com>
2023-10-11 21:11:29 -0400

    domain

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 function domainName() {
-    let a=0;
-    let b=0;
-    let c=0
-
     let pronoun = ['the', 'our'];
     let adj = ['great','big'];
+    let nouns = ['jogger', 'racoon'];
+    let domain = "";
+    let a=0;
+    let b=0;
+    let c=0;
+    
+    for (let a=0;a<pronoun.length;a++)
+    {
+        for (let b=0;b<adj.length;b++)
+        {
+            for (let c=0;c<nouns.length;c++)
+            {
+             domain += pronoun[a]+adj[b]+nouns[c]+".com <br>";
+
+     
+            }
+        }
+    }
+return domain;
 }
+document.querySelector("#btn").addEventListener ("click", () => {
+    document.querySelector("#name").innerHTML = domainName();
+
+
+
+
+});
